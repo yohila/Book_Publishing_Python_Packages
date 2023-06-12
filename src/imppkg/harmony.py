3631ab5b063d668350885a7885acc65d11fd2148
yohila <awais_hec@yahoo.com>
2023-06-12 15:04:01 +0200

    Testing Python Package for Summer School

---
+++ src/imppkg/harmony.py
@@ -0,0 +1,24 @@
+import sys
+
+from imppkg.harmonic_mean import harmonic_mean
+
+
+def _parse_nums(inputs: list[str]) -> list[float]:
+    try:
+        return [float(num) for num in inputs]
+    except ValueError:
+        return []
+
+
+def _calculate_result(nums: list[float]) -> float:
+    try:
+        return harmonic_mean(nums)
+    except ZeroDivisionError:
+        return 0.0
+
+
+
+# def main() -> None:
+#     nums = _parse_nums(sys.argv[1:])
+#     result = _calculate_result(nums)
+#     print(_format_output(result))
